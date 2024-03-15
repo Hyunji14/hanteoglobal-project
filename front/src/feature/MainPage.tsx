808d60cc721694f7260a01edb39c208bd7e823fb
[SW7] 정현지 <blackit14@gmail.com>
2024-03-15 21:04:51 +0900

    [FE] refact : 메인 페이지 수정

--- front/src/feature/MainPage.tsx
+++ front/src/feature/MainPage.tsx
@@ -1,11 +1,18 @@
+import { Suspense } from 'react';
+
 import Category from '@/components/Category/Category';
-import ChartPage from './chart/ChartPage';
+import Banner from '@/components/Banner/Banner';
+import Contents from '@/components/Contents/Contents';
+import { Loading } from '@/components/Loading';
 
 export default function MainPage() {
   return (
     <>
-      <Category categoryId='chart' />
-      <ChartPage />
+      <Suspense fallback={<Loading />}>
+        <Category categoryId='chart' />
+        <Banner categoryId='chart' />
+        <Contents categoryId='chart' />
+      </Suspense>
     </>
   );
 }
