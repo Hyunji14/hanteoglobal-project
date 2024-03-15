95cf07d35701457267f2f4fa86715691c8c6c216
[SW7] 정현지 <blackit14@gmail.com>
2024-03-15 20:27:55 +0900

    [FE] feat : #17 빈 화면 페이지 추가

---
+++ front/src/components/Empty/Empty.styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 700px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: ${({ theme }) => theme.colors.darkgray};
+  font-size: ${({ theme }) => theme.size.lg}px;
+  font-weight: 700;
+`;
